Dreistein/vlc_history_shuffle

commit 10c94a93bfdb488a34bb52ebda8f007c4e2de098

Fix issue with files containing commas
The CSV data file uses commas as separator. The data file contains paths to indexed songs.
It is not common to use commas in files but it can happen. Because of this, some files get loaded
incorrectly and thus not updated across restarts of the extension.

This is now fixed as this version will look for the separator from the end of the line
instead of the beginning where the path is stored.

history_playlist.lua
@@ -173,6 +173,18 @@ function randomize_playlist( )
 	vlc.playlist.play()
 end
 
+-- finds the last occurence of findString in mainString
+-- and returns the index
+-- otherwise nil if not found
+function find_last(mainString, findString)
+    local reversed = string.reverse(mainString)
+    local last = string.find(reversed, findString)
+    if last == nil then
+        return nil
+    end
+    return #mainString - last + 1
+end
+
 -- -- IO operations -- --
 
 -- Loads the data from
@@ -193,12 +205,12 @@ function load_data_file()
 		-- file successfully opened
 		for line in file:lines() do
 			-- csv layout is `path,like,timestamp`
-			local num_split = string.find(line, ",")
-			local path = string.sub(line, 1, num_split-1)
+			local num_split = find_last(line, ",")
+			local date = string.sub(line, 1, num_split-1)
 			
-			line = string.sub(line, num_split+1)
-			num_split = string.find(line, ",")
-			local like = tonumber(string.sub(line, 1, num_split-1))
+			line = string.sub(line, 0, num_split-1)
+			num_split = find_last(line, ",")
+			local path = tonumber(string.sub(line, 1, num_split-1))
 			local date = tonumber(string.sub(line, num_split+1))
 
 			if like == nil then
@@ -289,6 +301,8 @@ function playing_changed()
 	  	save_data_file()
 	end
 end
+
+function meta_changed() end
 
 -- -- Queue implementation -- --
 -- Idea from https://www.lua.org/pil/11.4.html

commit ea1f58e0545ca7f822a554af52b63834e07989f5

Fix date getting updated on reload
The string substitution didn't work properly and thus
the date was nil and got set to the current time every time
the history data file was loaded.

So no song does ever get positive points added.
This is now fixed by correcting the string substitution.

The issue was likely introduced in 10c94a93bfdb488a34bb52ebda8f007c4e2de098

history_playlist.lua
@@ -58,10 +58,6 @@ function init_playlist( )
 
 	-- load playlist items from file
 	load_data_file()
-	local count = 0
-	for _ in pairs(store) do count = count + 1 end
-	---store = vlc.config.get('store')
-	vlc.msg.info(prefix .. "store has size " .. count)
 
 	local time = os.time() -- current time for comparison of last played
 	local playlist = vlc.playlist.get("playlist",false).children
@@ -213,7 +209,11 @@ function load_data_file()
 		for line in file:lines() do
 			-- csv layout is `path,like,timestamp`
 			local num_split = find_last(line, ",")
-			local date = tonumber(string.sub(line, 1, num_split-1))
+			local date = tonumber(string.sub(line, num_split+1))
+
+			if date == nil then
+				vlc.msg.warn(prefix .. "date nil: " .. line .. " => " .. string.sub(line, 1, num_split-1))
+			end
 			
 			line = string.sub(line, 0, num_split-1)
 			num_split = find_last(line, ",")