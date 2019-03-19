Dreistein/vlc_history_shuffle

commit 10c94a93bfdb488a34bb52ebda8f007c4e2de098

Fix issue with files containing commas
The CSV data file uses commas as separator. The data file contains paths to indexed songs.
It is not common to use commas in files but it can happen. Because of this, some files get loaded
incorrectly and thus not updated across restarts of the extension.

This is now fixed as this version will look for the separator from the end of the line
instead of the beginning where the path is stored.

history_playlist.lua
@@ -173,6 +173,18 @@ function randomize_playlist( )
 	vlc.playlist.play()
 end
 
+-- finds the last occurence of findString in mainString
+-- and returns the index
+-- otherwise nil if not found
+function find_last(mainString, findString)
+    local reversed = string.reverse(mainString)
+    local last = string.find(reversed, findString)
+    if last == nil then
+        return nil
+    end
+    return #mainString - last + 1
+end
+
 -- -- IO operations -- --
 
 -- Loads the data from
@@ -193,12 +205,12 @@ function load_data_file()
 		-- file successfully opened
 		for line in file:lines() do
 			-- csv layout is `path,like,timestamp`
-			local num_split = string.find(line, ",")
-			local path = string.sub(line, 1, num_split-1)
+			local num_split = find_last(line, ",")
+			local date = string.sub(line, 1, num_split-1)
 			
-			line = string.sub(line, num_split+1)
-			num_split = string.find(line, ",")
-			local like = tonumber(string.sub(line, 1, num_split-1))
+			line = string.sub(line, 0, num_split-1)
+			num_split = find_last(line, ",")
+			local path = tonumber(string.sub(line, 1, num_split-1))
 			local date = tonumber(string.sub(line, num_split+1))
 
 			if like == nil then
@@ -289,6 +301,8 @@ function playing_changed()
 	  	save_data_file()
 	end
 end
+
+function meta_changed() end
 
 -- -- Queue implementation -- --
 -- Idea from https://www.lua.org/pil/11.4.html

commit b96b7cadb088641da0d37ef04adffff4ca99a290

Fix store not getting loaded properly
The store didn't get properly populated because the path was attempted to be
converted to a number (which doesn't work and returns a nil value)
The subsequent check if path is of any value failed and the sore doesn't get the
new entry.

This is fixed by simply removing the tonumber. Also the date now get's converted
to a number.

The issue was likely introduced in 10c94a93bfdb488a34bb52ebda8f007c4e2de098

history_playlist.lua
@@ -1,6 +1,7 @@
 
 local played = {} -- holds all music items form the current playlist
 local store = {}	-- holds all music items from the database
+
  -- used in playing_changed
  -- the event gets triggered multiple times and we don't want to
  -- set the rating down multiple times
@@ -57,6 +58,10 @@ function init_playlist( )
 
 	-- load playlist items from file
 	load_data_file()
+	local count = 0
+	for _ in pairs(store) do count = count + 1 end
+	---store = vlc.config.get('store')
+	vlc.msg.info(prefix .. "store has size " .. count)
 
 	local time = os.time() -- current time for comparison of last played
 	local playlist = vlc.playlist.get("playlist",false).children
@@ -203,15 +208,17 @@ function load_data_file()
 		end
 	else
 		-- file successfully opened
+		vlc.msg.info(prefix .. "data file successfully opened")
+		local count = 0
 		for line in file:lines() do
 			-- csv layout is `path,like,timestamp`
 			local num_split = find_last(line, ",")
-			local date = string.sub(line, 1, num_split-1)
+			local date = tonumber(string.sub(line, 1, num_split-1))
 			
 			line = string.sub(line, 0, num_split-1)
 			num_split = find_last(line, ",")
-			local path = tonumber(string.sub(line, 1, num_split-1))
-			local date = tonumber(string.sub(line, num_split+1))
+			local path = string.sub(line, 1, num_split-1)
+			local like = tonumber(string.sub(line, num_split+1))
 
 			if like == nil then
 				like = 100
@@ -221,9 +228,11 @@ function load_data_file()
 				date = os.time()
 			end
 			if path then
+				count = count + 1
 				store[path] = {like=like, time=date}
 			end
 		end
+		vlc.msg.info(prefix .. "processed " .. count)
 	end
 	io.close(file)
 end